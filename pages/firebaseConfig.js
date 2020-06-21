7d3fe90bb0f83465350870d46e241b28efda15d9
nevzatkaya <nevzatkaya@gmail.com>
2020-06-21 23:17:27 +0300

    Delete firebaseConfig.js

--- pages/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyCgyOIdPMxNmQeO4GdMxJuEuXeoXKC9lD4",
-  authDomain: "neovuetodo.firebaseapp.com",
-  databaseURL: "https://neovuetodo.firebaseio.com",
-  projectId: "neovuetodo",
-  storageBucket: "neovuetodo.appspot.com",
-  messagingSenderId: "696985211416",
-  appId: "1:696985211416:web:e9f44cdafa91ec0153a96d"
-};
